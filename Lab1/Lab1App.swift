052252fc3634d8fb1894e2216e57de79e982a792
bl <asdfghjk@sdfgh.pl>
2023-10-03 14:35:08 +0200

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
